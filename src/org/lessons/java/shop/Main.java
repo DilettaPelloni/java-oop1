609e32d25eb25403e01243eaf4f70cfe4b12421a
Diletta Pelloni <dilpel@live.it>
2023-06-05 14:25:58 +0200

    Completato esercizio base

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,35 @@
+package org.lessons.java.shop;
+
+//IMPORT
+import java.text.DecimalFormat;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //creo una nuova istanza di Prodotto
+        Prodotto product1 = new Prodotto("pasta", "pasta fresca", 5.2, 22);
+
+        //Stampo gli attributi di product1 usando i getters
+        System.out.println("Codice: " + product1.getCodice());
+        System.out.println("Nome: " + product1.getNome());
+        System.out.println("Descrizione: " + product1.getDescrizione());
+        System.out.println("Prezzo: " + new DecimalFormat("###,###.00€").format(product1.getPrezzo()));
+        System.out.println("Aliquota IVA: " + product1.getIva() + "%");
+        //stampo anche il prezzo ivato e il nome completo
+        System.out.println("Prezzo ivato: " + new DecimalFormat("###,###.00€").format(product1.getPrezzoIvato()));
+        System.out.println("Nome completo: " + product1.getNomeCompleto());
+
+        //spazio
+        System.out.println("\n---\n");
+
+        //Modifico i campi modificabili
+        product1.setNome("pane");
+        product1.setDescrizione("pane caldo");
+        product1.setPrezzo(10.3);
+        product1.setIva(10);
+
+        //stampo gli attributi usando il metodo stampaAttibuti
+        product1.stampaAttibuti();
+
+    }
+}
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,2 +1,72 @@
-package org.lessons.java.shop;public class Prodotto {
+package org.lessons.java.shop;
+//IMPORT
+import java.text.DecimalFormat;
+import java.util.Random;
+
+public class Prodotto {
+
+    //ATTRIBUTI
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+    private int iva;
+
+    //COSTRUTTORE
+    public Prodotto(String nome, String descrizione, double prezzo, int iva) {
+        this.codice = new Random().nextInt(0, 100000000);
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    //GETTERS
+    public int getCodice() {
+        return codice;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getDescrizione() {
+        return descrizione;
+    }
+    public double getPrezzo() {
+        return prezzo;
+    }
+    public int getIva() {
+        return iva;
+    }
+
+    //SETTERS
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+
+    //METHODS
+    public double getPrezzoIvato() {
+        return prezzo + prezzo * iva / 100;
+    }
+    public String getNomeCompleto() {
+        return codice + "-" + nome;
+    }
+    public void stampaAttibuti() {
+        System.out.println("Codice: " + codice);
+        System.out.println("Nome: " + nome);
+        System.out.println("Descrizione: " + descrizione);
+        System.out.println("Prezzo: " + new DecimalFormat("###,###.00€").format(prezzo));
+        System.out.println("Aliquota IVA: " + iva + "%");
+        System.out.println("Prezzo ivato: " + new DecimalFormat("###,###.00€").format(getPrezzoIvato()));
+        System.out.println("Nome completo: " + getNomeCompleto());
+    }
+
 }
